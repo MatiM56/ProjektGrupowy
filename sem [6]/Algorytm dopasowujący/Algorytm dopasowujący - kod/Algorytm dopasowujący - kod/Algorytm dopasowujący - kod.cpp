088f47d41c0db057e7bd1ea145c3386c58288d9c
achterpik <s198010@student.pg.edu.pl>
2026-03-12 13:19:15 +0100

    Dodanie pomysłu rozwiązania

---
+++ sem [6]/Algorytm dopasowujący/Algorytm dopasowujący - kod/Algorytm dopasowujący - kod/Algorytm dopasowujący - kod.cpp
@@ -0,0 +1,30 @@
+﻿// Algorytm dopasowujący - kod.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main() // dostaje dane
+{
+    const int N = 3;   //ilość BSSID branych pod uwagę
+    const int M = 3;   //ilość pkt zwróconych z serwera
+
+     string zbiorBSSID[N];              // dane z Rasberry
+     float zbiorRSSI[N];
+
+     string zbiorBSSIDwzorzec[M][N];       // dane z serwera
+     float zbiorRSSIwzorzec[M][N];
+
+     /*Wyliczenie błędów*/
+     for (int i = 0; i < M; i++)
+     {
+
+     }
+
+     return 0;
+}
+
+
